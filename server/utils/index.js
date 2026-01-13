bd0ed8778dba201216becd588d107d5407b5dbbf
Khushiiii <147794609+khushisharmaa00@users.noreply.github.com>
2026-01-13 18:33:55 +0530

    Refactor JWT cookie settings and imports
    Updated JWT cookie settings for production environment.

--- server/utils/index.js
+++ server/utils/index.js
@@ -35,13 +35,14 @@ export const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
-
+const isProduction = process.env.NODE_ENV !== "development";
   // Change sameSite from strict to none when you deploy your app
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "lax", //prevent CSRF attack
+    secure: true,
+    sameSite: "none", //prevent CSRF attack
     maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
   });
   return token;
 };
+
